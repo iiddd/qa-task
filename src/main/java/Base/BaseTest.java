4ac3939293f031ddeeac992f9b3bf20d19b12e22
Andrey Merkulov <amerkulov@wiley.com>
2018-09-28 19:56:56 +0300

    gecko driver support for mac and win added

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -9,7 +9,9 @@ public class BaseTest {
 
     private WebDriver driver;
     private static final String CHROMEDRIVER_PATH_PROPERTY = "webdriver.chrome.driver";
+    private static final String GECKODRIVER_PATH_PROPERTY = "webdriver.gecko.driver";
     private static final String CHROMEDRIVER_MAC_PATH = "src/main/resources/drivers/mac/chromedriver";
+    private static final String GECKODRIVER_MAC_PATH = "src/main/resources/drivers/mac/geckodriver";
     private static final String OS_NAME_PROPERTY = "os.name";
     private static final String MAC_OS_X_PLATFORM_NAME = "Mac OS X";
     private static final String BROWSER_PROPERTY = "browser";
@@ -35,10 +37,16 @@ public class BaseTest {
             return driver;
         }
         if (getProfileBrowserProperty().equals(FF_DRIVER_PROPERTY)) {
+            if (isMac()) {
+                System.setProperty(GECKODRIVER_PATH_PROPERTY, GECKODRIVER_MAC_PATH);
+            }
             driver = new FirefoxDriver();
             return driver;
         } else {
             //Chrome driver will be initialized as default webdriver in case profile is not selected
+            if (isMac()) {
+                System.setProperty(CHROMEDRIVER_PATH_PROPERTY, CHROMEDRIVER_MAC_PATH);
+            }
             return new ChromeDriver();
         }
     }
