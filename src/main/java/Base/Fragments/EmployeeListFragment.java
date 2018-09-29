a941761e1679b4fe7f4f926d602a7ffba9cb0ad9
Andrey Merkulov <merkulov.andrej@gmail.com>
2018-09-29 19:04:03 +0300

    EmployeeListFragment draft is created

---
+++ src/main/java/Base/Fragments/EmployeeListFragment.java
@@ -0,0 +1,11 @@
+package Base.Fragments;
+
+/**
+ * This class is used to store common methods for Employee list tests
+ */
+
+public class EmployeeListFragment {
+
+    public void createEmployee() {
+    }
+}
